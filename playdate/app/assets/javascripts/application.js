79c7ee47a8ab3d380c8813f77da4178aa7846ac2
GWEN <gwenmak@gmail.com>
2015-06-04 17:40:02 -0400

    chosen gem doesn't work

--- playdate/app/assets/javascripts/application.js
+++ playdate/app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require turbolinks
 //= require underscore
 //= require bootstrap-sprockets
+
 //= require backbone
 //= require backbone_rails_sync
 //= require backbone_datalink
--- playdate/config/routes.rb
+++ playdate/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :students
   devise_for :admins
   root to: 'application#index'
   resources :students
   resources :classrooms
   devise_for :users
+
+  # mailbox folder routes
+  get "mailbox/inbox" => "mailbox#inbox", as: :mailbox_inbox
+  get "mailbox/sent" => "mailbox#sent", as: :mailbox_sent
+  get "mailbox/trash" => "mailbox#trash", as: :mailbox_trash
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
