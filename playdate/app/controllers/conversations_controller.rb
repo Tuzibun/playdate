e734298bce29873d53384de2ca687dee53b26254
GWEN <gwenmak@gmail.com>
2015-06-07 10:05:48 -0400

    all well in rails

--- playdate/app/controllers/conversations_controller.rb
+++ playdate/app/controllers/conversations_controller.rb
@@ -14,7 +14,7 @@ class ConversationsController < ApplicationController
   def show
     @receipts = conversation.receipts_for(current_user).order("created_at ASC")
     # mark conversation as read
-    conversation.mark_as_read(current_user)
+    # conversation.mark_as_read(current_user)
   end
 
 
